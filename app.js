2e0a59cd5fea3517b99fa2245ca2b0bb6ec4b386
Warren Ng <warrenng23@gmail.com>
2016-02-17 13:23:33 -0800

    adds initial applicants route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var routes = require('./routes/index');
-var users = require('./routes/applicant');
+var applicants = require('./routes/applicants');
 
 var app = express();
 
@@ -25,8 +25,9 @@ app.use(session({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', routes);
-app.use('/users', users);
+app.use('/applicants', applicants);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
