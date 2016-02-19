b8010e0e140057b0844a0fbc614088c034a102a5
Warren Ng <warrenng23@gmail.com>
2016-02-19 13:46:27 -0800

    adds db interface and session usage for GET / route

--- db/index.js
+++ db/index.js
@@ -1,23 +1,99 @@
 var knex = require('knex')({
   client: 'sqlite3',
   connection: {
-    filename: './db/development.sqlite3',
+    filename: './db/test.sqlite3',
   },
-  useNullAsDefault: true
+  useNullAsDefault: false
 });
+
+var noOp = function() {};
 
 var db = {
   init: function(callback) {
+    callback = callback || noOp;
+    console.log('Creating tables...');
     knex.schema.createTableIfNotExists('shopper_applicants', function(table) {
       table.increments('id');
       table.string('first_name');
       table.string('last_name');
       table.string('email');
+      table.string('city');
       table.string('phone_number');
       table.boolean('completed');
+      table.boolean('background_check');
       table.timestamps();
+    })
+    .then(function() {
+      console.log('Adding initial test user...');
+      knex('shopper_applicants').insert({
+        first_name: 'Dead',
+        last_name: 'Pool',
+        email: 'dead.pool@marvel.com',
+        phone_number: '3211234567',
+        completed: false,
+        created_at: Date.now(),
+        updated_at: Date.now(),
+      })
+      .then(function(user) {
+        console.log('did user add', user);
+      })
+      .catch(function(err) {
+        // Dump errors into database if applicable..
+        console.error('err', err);
+      })
+    })
+    .catch(function(err) {
+      console.log('err', err);
+      callback();
     });
-    callback();
+  },
+  updateOrInsert: function(user, callback) {
+    callback = callback || noOp;
+    knex.transaction(function(trx) {
+      knex('shopper_applicants')
+        .where({
+          email: user.email
+        })
+        .transacting(trx)
+        .then(function(users) {
+          console.log('users: ', users);
+          if (!users.length) {
+            console.log('inserting..');
+            return trx
+                    .insert(user)
+                    .into('shopper_applicants');
+          }
+          return trx
+            .select('*')
+            .from('shopper_applicants')
+            .where({
+              email: user.email
+            })
+            .update(user);
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .then(function() {
+      console.log('updateOrInsert complete...');
+      callback(null, user);
+    })
+    .catch(function(err) {
+      console.log('updateOrInsert error: ', err);
+      callback(err);
+    });
+  },
+
+  query: function(user, callback) {
+    callback = callback || noOp;
+    knex('shopper_applicants')
+      .where(user)
+      .then(function(users) {
+        return callback(null, users);
+      })
+      .catch(function(err) {
+        return callback(err);
+      });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,65 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
 var ReactServer = require('react-dom/server');
 var React = require('react');
 var AppContainer = require('../src/main/AppContainer').default;
+var db = require('../db');
 
 router.get('/', function(req, res) {
-  // const user = {
-  //   first_name: 'Warren',
-  //   last_name: 'Ng',
-  //   phone_number: 6466888186,
-  //   city: 'san_francisco'
-  // };
-  const user = {};
-  res.write(`
-    <!DOCTYPE html>
-      <html>
-      <head>
-        <title>Shopper</title>
-        <link rel="stylesheet" href="/css/bundle.css" />
-      </head>
-      <body>
-        <div id="app">`
-  );
-  res.write(
-    ReactServer.renderToString(
-      React.createElement(AppContainer, { data: user })
-    )
-  );
-  res.write('</div><script id="data" type="json">')
-  const jsonString = JSON.stringify(user);
-  console.log(jsonString);
-  res.write(jsonString);
-  res.write(`
-        </script>
-        <script src="/js/bundle.js"></script>
-      </body>
-    </html>`);
-  res.end();
+  var series = [];
+  var user = {};
+  var session = req.session;
+  if (session.email) {
+    series.push(function(callback) {
+      db.query({
+        email: session.email
+      }, function(err, users) {
+        if (err) {
+          return callback(err);
+        }
+        if (users.length) {
+          user = users[0];
+        }
+
+        callback();
+      });
+    });
+  }
+
+  series.push(function(callback) {
+    res.write(`
+      <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Shopper</title>
+          <link rel="stylesheet" href="/css/bundle.css" />
+        </head>
+        <body>
+          <div id="app">`
+    );
+    res.write(
+      ReactServer.renderToString(
+        React.createElement(AppContainer, { data: user })
+      )
+    );
+    res.write('</div><script id="data" type="json">')
+    const jsonString = JSON.stringify(user);
+    res.write(jsonString);
+    res.write(`
+          </script>
+          <script src="/js/bundle.js"></script>
+        </body>
+      </html>`);
+    callback();
+  });
+
+  async.series(series, function(err) {
+    if (err) {
+      return console.error(err);
+    }
+    res.end();
+  });
 });
 
 module.exports = router;
